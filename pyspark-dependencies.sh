cd15e555e8aa871d535e6a3ee9261c0934cee3dd
AJITHSELVAM <112960431+ajithselvam@users.noreply.github.com>
2025-02-26 13:10:23 +0530

    Create pyspark-dependencies.sh

---
+++ pyspark-dependencies.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+
+
+
+osascript -e 'tell application "Terminal"
+    do script "brew install openjdk@11 && brew install pyenv && pyenv install 3.11.5 && brew install pyenv-virtualenv && pyenv virtualenv 3.11.5 devenv
+ && pyenv shell devenv && pyenv shell devenv && brew install python "
+    activate
+end tell'
